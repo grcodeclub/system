59b80c6ae32d0bc3c873cd22a4ad2403c921be8e
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-22 14:26:53 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/system">Λειτουργικό Σύστημα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = ' <li><a href="https://grcodeclub.github.io/system/orologia">Ορολογία</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/linux">Linux</li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/android">Android</li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
