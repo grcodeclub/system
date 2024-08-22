1cc1f536df751e0e972b352ae0713ddfd188af0e
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-22 14:28:45 +0300

    Add files via upload

---
+++ android/js/menu.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android">Android</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android/versions">Eκδόσεις</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android/more">Περισσότερα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+
+    menuLink = ' <li><a href="https://grcodeclub.github.io/android">Λειτουργικό Σύστημα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+  });
+  
