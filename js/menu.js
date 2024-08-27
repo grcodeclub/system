c4881fb670a4b1b08afb5b64a4227955bfc9b2b6
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-27 13:30:24 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
-$(document).ready(function() {
-    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/system">Λειτουργικό Σύστημα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = ' <li><a href="https://grcodeclub.github.io/system/orologia">Ορολογία</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/linux">Linux</li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/system/android">Android</li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li style="height: 200px;"></li>';
-    $('.sidebarMenuInner').append(menuLink);
-  });
+window.onload = function() {
+    var menuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.github.io/system">Λειτουργικό Σύστημα</a></li>',
+        '<li><a href="https://grcodeclub.github.io/system/orologia">Ορολογία</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>',
+        '<li><a href="https://grcodeclub.github.io/linux">Linux</li>',
+        '<li><a href="https://grcodeclub.github.io/system/android">Android</li>',
+        '<li style="height: 200px;"></li>';
+    ];
+    
+    var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+    $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+    menuLinks.forEach(function(link) {
+        $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+    });
+});
+};
